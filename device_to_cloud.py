fa42d1e8ac14783f842761a63589f5a399b6c793
pmanlukas <lukas.pollmann@tum.de>
2018-01-18 16:49:27 +0100

    add send to cloud

---
+++ device_to_cloud.py
@@ -0,0 +1,6 @@
+import random
+import time
+import sys
+import iothub_client
+from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
+from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
